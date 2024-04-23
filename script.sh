25403f5f5b40f06b387ffd61413a54c1546ac89d
JayendraKambhampati <k.jayendra67@gmail.com>
2024-04-23 16:28:36 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,3 +9,43 @@ echo "Running save_monthly_script_path($MONTH,$YEAR)function:"
 psql postgresql://ippuser:postgres@localhost/ipp_logs << EOF
      select from save_monthly_script_path($MONTH,$YEAR);
 EOF
+
+
+
+
+
+
+
+
+#!/bin/sh
+
+# Function to send email notification
+send_email_notification() {
+    TO="rama.kambhampati@optum.com"
+    SUBJECT="Regular Notification"
+    BODY="This is a regular notification sent via script execution."
+    
+    echo "$BODY" | mail -s "$SUBJECT" "$TO"
+}
+
+MONTH=$(date -d "-1 month" +%m)
+YEAR=2024
+
+# Function to execute
+execute_function() {
+    echo "Running save_monthly_script_path($MONTH,$YEAR) function:"
+    psql postgresql://ippuser:postgres@localhost/ipp_logs << EOF
+    select from save_monthly_script_path($MONTH,$YEAR);
+EOF
+}
+
+# Execute the function
+execute_function
+
+# Check if script is running from a terminal
+if tty -s; then
+    echo "Script is not running from cron."
+    send_email_notification
+else
+    echo "Script is running from cron."
+fi
